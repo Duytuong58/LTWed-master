4312beee318d9695d5c94bee08777d3658fd19ef
Tưởng Nguyễn Duy <nguyenduytuongc3@gmail.com>
2020-06-10 23:36:38 +0700

    Update 10/6

---
+++ BigShool1/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BigShool1.Models
+{
+    internal class ApplicationDbContext
+    {
+        public object Categories { get; internal set; }
+        public object Courses { get; internal set; }
+
+        internal void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BigShool1/ViewModels/CourseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using BigSchool1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigShool1.ViewModel
+{
+    public class CourseViewModel
+    {
+        [Require]
+        public string Placce { get; set; }
+        [Require]
+        public string Date { get; set; }
+        [Require]
+        public string Time { get; set; }
+        [Require]
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public string Place { get; internal set; }
+
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}
